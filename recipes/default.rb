9c7111325ab9edb851cb9c6e7728d85a86966d82
Thomas Boerger <tboerger@suse.de>
2015-03-17 11:46:14 +0100

    Refresh the repos after added

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,7 +26,7 @@ when "suse"
     key node["nvidia"]["zypper"]["key"]
     title node["nvidia"]["zypper"]["title"]
 
-    action :add
+    action [:add, :refresh]
 
     only_if do
       node["nvidia"]["zypper"]["enabled"]
